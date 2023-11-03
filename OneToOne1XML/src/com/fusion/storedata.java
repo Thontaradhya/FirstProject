126a705fdf577fbd25450d3660f20070a2213070
devopscloud <tradhya691@gmail.com>
2023-11-03 08:37:08 +0530

    First Commit

---
+++ OneToOne1XML/src/com/fusion/storedata.java
@@ -0,0 +1,56 @@
+package com.fusion;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+
+public class storedata {
+
+	public static void main(String[] args) {
+		
+		try {
+		SessionFactory factory = new Configuration().configure("employee.config.xml").buildSessionFactory();
+    Session session = factory.openSession();  
+    Transaction t = session.beginTransaction(); 
+      
+     Employee e1=new Employee();    
+        
+     e1.setName("rakshith");
+      e1.setEmail("r@1234");
+      
+      Address ad=new Address();
+      
+      ad.setAddressLine1("sadashivanagara");    
+      ad.setCity("Banglore");    
+      ad.setState("Karnataka");    
+      ad.setCountry("India");    
+      ad.setPincode(512998);
+       
+      e1.setAddress(ad);
+       ad.setEmployee(e1); 
+        
+   
+    
+    session.persist(e1);    
+    t.commit();    
+        
+    session.close(); 
+    
+    
+     
+     System.out.println("Success");
+     
+     
+     
+}  
+		catch(Exception e) {
+			
+			System.out.print("Exception"+e);
+		}
+
+	}
+
+}
